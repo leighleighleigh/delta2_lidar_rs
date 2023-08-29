31263bb20a78091f17c40229daa2f4e332f52169
Leigh Oliver <leighcharlesoliver@gmail.com>
2023-08-29 21:34:04 +1000

    ROS examples

--- examples/stream-ros2.py
+++ examples/stream-ros2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from time import sleep
 import numpy as np
+import math
 from typing import List, Tuple
 
 from delta2_lidar import Lidar, MeasurementFrame, FullScan
@@ -9,7 +10,7 @@ import rclpy
 from rclpy.node import Node
 from rclpy.duration import Duration
 from rclpy.time import Time
-from std_msgs.msg import Header, String
+from std_msgs.msg import Header, String, Float32
 from sensor_msgs_py.point_cloud2 import create_cloud_xyz32, PointCloud2
 
 def to_3D(points: List[Tuple[float,float]]) -> np.ndarray:
@@ -20,6 +21,7 @@ def timer_callback():
     global node
     global dev
     global pub
+    global pubrpm
     node.get_logger().info("Scanning")
 
     if dev.alive():
@@ -32,23 +34,32 @@ def timer_callback():
 
         # convert the 2D points to 3D
         points_xyz = to_3D(scan.points)
+
         hd = Header()
-        hd.frame_id = "delta2"
+        hd.frame_id = "cloud"
         hd.stamp = Time(seconds=scan.timestamp // 1e9, nanoseconds=scan.timestamp % 1e9).to_msg()
+
         pclmsg = create_cloud_xyz32(hd, points_xyz)
         pub.publish(pclmsg)
+
+        rpmmsg = Float32()
+        rpmmsg.data = float(scan.rpm)
+        pubrpm.publish(rpmmsg)
 
 def main(args=None):
     rclpy.init(args=args)
     global node
     global dev 
     global pub
+    global pubrpm
+    global pubscan
     # connect to the lidar
     dev = Lidar()
     dev.open("/dev/ttyUSB0")
     node = Node('delta2')
     node.create_timer(0.01, timer_callback)
-    pub = node.create_publisher(PointCloud2, "/scan", 0)
+    pub = node.create_publisher(PointCloud2, "/scanner/cloud", 0)
+    pubrpm = node.create_publisher(Float32, "/rpm", 0)
     rclpy.spin(node)
     rclpy.shutdown()
 
